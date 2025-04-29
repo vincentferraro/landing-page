f5ea868c11d3796c243643b225b5921a9d4d3a18
vincentferraro <v.ferraro@live.fr>
2025-04-29 11:08:51 +0200

    feat: Envoi d email possible avec nodeMailer

---
+++ src/app/api/mail/contact/route.ts
@@ -0,0 +1,48 @@
+// src/app/api/mail/contact/route.ts
+import { NextResponse } from 'next/server';
+import * as nodemailer from 'nodemailer';
+
+export async function POST(req: Request) {
+  const data = await req.json();
+  const { nom, prenom,phone, email, message } = data;
+
+  // Création du transporteur avec Ethereal
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.ethereal.email',
+    port: 587,
+    auth: {
+      user: process.env.ETHEREAL_USER,
+      pass: process.env.ETHEREAL_PASS,           
+    },
+  });
+
+  const mailBody = `
+                    📩 Nouveau message reçu depuis le site internet
+
+                    👤 Informations du contact :
+                    - Nom : ${nom}
+                    - Prénom : ${prenom}
+                    - Téléphone : ${phone}
+                    - Email : ${email}
+
+                    📝 Message :
+                    ${message}
+
+                    ---
+                    Ce message a été envoyé automatiquement depuis le formulaire de contact du site.
+                    `;
+  
+  try {
+    const info = await transporter.sendMail({
+      from: `"${prenom} ${nom}" <${email}>`,
+      to: process.env.ETHERAL_EMAIL,
+      subject: 'Nouveau message via le formulaire de contact',
+      text: mailBody,
+    });
+
+    return NextResponse.json({ success: true, messageId: info.messageId });
+  } catch (error) {
+    console.error('Erreur envoi mail :', error);
+    return NextResponse.json({ success: false, error: 'Erreur envoi email' }, { status: 500 });
+  }
+}
--- src/app/hooks/UseContactForm.ts
+++ src/app/hooks/UseContactForm.ts
@@ -21,12 +21,32 @@ export const useContactForm = () => {
       email: Yup.string().email("Email invalide").required("Champ requis"),
       message: Yup.string().required("Champ requis"),
     }),
-    onSubmit: (values) => {
+    onSubmit: async (values) => {
       setSubmitAttempted(true);
       if (!formik.isValid) return;
 
       console.log("Envoi du formulaire :", values);
+      
       // Appel de l'API ou fonction nodemailer
+      try{
+        const response = await fetch(`/api/mail/contact`,{
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(values)
+        })
+        const result = await response.json()
+
+        if(response.ok){
+          console.log('success', result)
+          formik.resetForm()
+        }else{
+          throw new Error(result.error || "Erreur inconnue")
+        }
+        
+      }catch(err){
+        console.error('Erreur lors de l’envoi du formulaire:', err);
+    }
+      
     },
   });
 
